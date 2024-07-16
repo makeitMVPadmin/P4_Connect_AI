139dc6cfe98607017ca781789b9375a50b543a4c
Gloria Lau <glorialau@live.ca>
2024-07-16 15:39:58 -0700

    GL-set up App.js and Prompt Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import PromptPage from "./pages/PromptPage/PromptPage";
 
 function App() {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/prompt" element={<PromptPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/PromptPage/PromptPage.jsx
@@ -0,0 +1,11 @@
+import "./PromptPage.scss";
+
+const PromptPage = () => {
+  return(
+    <div className="promptpage-container">
+        <div className="promptpage">I am a prompt page</div>
+    </div>
+  )
+};
+
+export default PromptPage;
