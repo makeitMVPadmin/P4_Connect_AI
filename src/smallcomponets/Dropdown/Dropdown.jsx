eeef3e8b3b98ce7ad5a30dfec7c8ec0302363e9b
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-07-17 01:02:25 -0700

    components created

--- src/smallcomponets/Dropdown/Dropdown.jsx
+++ src/smallcomponets/Dropdown/Dropdown.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Dropdown.scss"
+import "./Dropdown.scss";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 const Dropdown = ({ dropDownInfo1, labelName }) => {
   const dropDownInfo = [
     "Design",
@@ -10,32 +11,60 @@ const Dropdown = ({ dropDownInfo1, labelName }) => {
     "Sales",
   ];
   const [updatedValue, setUpdatedValue] = React.useState();
-  const handleChange = (event) => {
-    setUpdatedValue(event.target.value);
-    
+  const [dummyValue, setDummyValue] = React.useState("Please select an option");
+  const [isActive, setIsActive] = React.useState(false);
+
+  const handleupdatedvalue = (event) => {
+    setUpdatedValue(event.target.innerHTML);
+    console.log(event.target.innerHTML);
+    setIsActive(!isActive);
+  };
+  const handledummyvalue = () => {
+    setUpdatedValue("");
+    setDummyValue("Please select an option");
+    setIsActive(!isActive);
+  };
+  const handledropdownclick = () => {
+    setIsActive(!isActive);
   };
   return (
     <div className="dropdown">
       <label className="dropdown__label" htmlFor="labelName">
         Hi{labelName}
       </label>
-      <select
-        className="dropdown__input"
-        onChange={handleChange}
-        value={updatedValue}
-        required
-      >
-        <option value="" >
-          Please select an option
-        </option>
-        {dropDownInfo.map((item, index) => {
-          return (
-            <option key={index} value={item}>
-              {item}
-            </option>
-          );
-        })}
-      </select>
+      <section className="dropdown_wrapper">
+        <article className="dropdown_wrapper_select">
+          <div className="dropdown_wrapper_select_trigger">
+            <span>{updatedValue ? updatedValue : dummyValue}</span>
+            <span class="arrow">
+              <ArrowDropDownIcon onClick={handledropdownclick} />
+            </span>
+          </div>
+          {isActive && (
+            <div className="dropdown_wrapper_select_option">
+              <span
+                className="custom-option"
+                data-value="Please Select an Option"
+                onClick={handledummyvalue}
+              >
+                Please select an option
+              </span>
+              {dropDownInfo.map((item, index) => {
+                return (
+                  <span
+                    key={index}
+                    className="custom-option"
+                    data-value={item}
+                    onClick={handleupdatedvalue}
+                  >
+                    {item}
+                  </span>
+                );
+              })}
+            </div>
+          )}
+        </article>
+      </section>
     </div>
   );
 };
