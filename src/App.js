e9e76660b688cc74c0eeba34bd5917f6a724b3e5
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-07-16 20:39:08 -0700

    components created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./main.js";
 import PromptPage from "./pages/PromptPage/PromptPage";
-
+import Dropdown from "./smallComponents/Dropdown";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/prompt" element={<PromptPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/prompt" element={<PromptPage />} />
+      </Routes>
+      <Dropdown/>
+    </>
   );
 }
 
