fdf0d153693c29dddc75667b66ab55d8717b5b59
Gloria Lau <glorialau@live.ca>
2024-07-16 17:45:47 -0700

    GL-add Home Page and link to Prompt Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./main.js";
 import PromptPage from "./pages/PromptPage/PromptPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/prompt" element={<PromptPage />} />
     </Routes>
   );
