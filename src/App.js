4687c709c7505f2feb9c9a4f794dc9dfe2c3d141
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2024-07-19 15:16:46 -0700

    Added DropdownCheckbox component with custom styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./main.js";
 import PromptPage from "./pages/PromptPage/PromptPage";
 import Dropdown from "./components/Dropdown/Dropdown.jsx";
+import DropdownCheckbox from "./components/DropdownCheckbox/DropdownCheckbox.jsx"
+
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/prompt" element={<PromptPage />} />
       </Routes>
       <Dropdown />
+      <DropdownCheckbox/>
+      
     </>
   );
 }
