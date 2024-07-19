fef1dced338ea4352b1a2f94d5248af9c9cc514e
MepilLeaf <77562979+MepilLeaf@users.noreply.github.com>
2024-07-19 02:14:42 -0700

    JH-remove testing code from App.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./main.js";
 import PromptPage from "./pages/PromptPage/PromptPage";
 
-import Button from "./components/Buttons/Button/Button.jsx";
-import PillButton from "./components/Buttons/PillButton/PillButton.jsx";
-
 function App() {
-  const ev = (x) => {
-    console.log(x);
-  };
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/prompt" element={<PromptPage />} />
       </Routes>
-      <Button
-        text="hello there"
-        color="yellow"
-        eventListener={() => {
-          ev("helo");
-        }}
-      />
-      <PillButton text="hello there hello there hello there" color="blue" />
     </>
   );
 }
