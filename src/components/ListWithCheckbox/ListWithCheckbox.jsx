26c69a4f661a664a75341a8dbb0c57dcc9d4b8f1
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-07-19 13:28:33 -0700

    List with Checkbox started

--- src/components/ListWithCheckbox/ListWithCheckbox.jsx
+++ src/components/ListWithCheckbox/ListWithCheckbox.jsx
@@ -1,30 +1,40 @@
 import React, { useState } from "react";
-
+import "./ListWithCheckbox.scss";
 const ListWithCheckbox = ({ options, label, onFunctionChange }) => {
-  const options1 = ["option1", "option2", "option3"];
+  const options1 = [
+    "Scheduled & attend one Accountability Meeting",
+    "Attend 2 accountability meeting",
+    "Attend 3 accountability meeting",
+    "Attend all Accountability meeting",
+  ];
   const [checked, setChecked] = useState(
     options1.reduce((arr, option) => {
       arr[option] = false;
-
       return arr;
     }, {})
   );
+  const [active, setActive] = useState(false);
   console.log(checked);
-
+  const handleChange = (option) => {
+    setChecked({ ...checked, [option]: !checked[option] });
+    if (checked[option]) setActive(true);
+    //else setActive(false);
+  };
   return (
     <div className="listwithcheckbox">
-      <h2>{label}</h2>
+      <h2>ListWithCheckbox{label}</h2>
       <div className="listwithcheckbox_options">
         {options1.map((option, index) => (
           <label key={index}>
             <input
               type="checkbox"
               checked={checked[option]}
-              onChange={() => {
-                setChecked({ ...checked, [option]: !checked[option] });
-              }}
+              onChange={() => handleChange(option)}
+              className={active===true ? "option_active" : "option_inactive"}
             />
-            {option}
+            <span className={active===true ? "option_active" : "option_inactive"}>
+              {option}
+            </span>
           </label>
         ))}
       </div>
