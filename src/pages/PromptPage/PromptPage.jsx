18cae74762aa8253e0e9336aabce8ac6ee026123
mohanhasti <mohankrish.97@gmail.com>
2024-07-21 01:19:26 -0400

    setup-promptpage-container

--- src/pages/PromptPage/PromptPage.jsx
+++ src/pages/PromptPage/PromptPage.jsx
@@ -6,14 +6,22 @@ import DropdownCheckbox from "../../components/DropdownCheckbox/DropdownCheckbox
 
 const PromptPage = () => {
   return (
-    <div className="promptpage-container">
-      <div className="promptpage">
-        I am a prompt page
-        <Dropdown />
-        <DropdownCheckbox />
-        <ListWithCheckbox />
-        <QuizPage />
+    // <div className="promptpage-container">
+    //   <div className="promptpage">
+    //     {/* <Dropdown />
+    //     <DropdownCheckbox />
+    //     <ListWithCheckbox />
+    //     <QuizPage /> */}
+    //   </div>
+    // </div>
+    <div className="promptpage">
+      <div className="promptpage__container">
+        <div className="promptpage__top-bar" />
+        <div className="promptpage__middle-container" />
+        <div className="promptpage__bottom-bar" />
       </div>
+       {/* <div className="groupItem" /> */}
+       {/* <div className="groupInner" />  */}
     </div>
   );
 };
