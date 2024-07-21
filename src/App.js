2346e0b28fff2408c3d4086f1cd88b8a6ebab69e
MepilLeaf <77562979+MepilLeaf@users.noreply.github.com>
2024-07-21 10:28:17 -0700

    change button styling to match design changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./main.js";
 import PromptPage from "./pages/PromptPage/PromptPage";
 
+import Button from "./components/Button/Button";
+
 function App() {
   return (
     <>
+      <Button color="white" text="hello" />
+      <Button color="blue" text="hello" />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/prompt" element={<PromptPage />} />
