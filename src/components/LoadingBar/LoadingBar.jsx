8978a2bb06c49520c22ac0b6a07dac3d20042e57
MepilLeaf <77562979+MepilLeaf@users.noreply.github.com>
2024-07-23 16:17:48 -0700

    JH-add base loading bar component code

---
+++ src/components/LoadingBar/LoadingBar.jsx
@@ -0,0 +1,11 @@
+import "./LoadingBar.scss";
+
+const LoadingBar = () => {
+  <div className="loading-bar">
+    <div className="loading-bar__box"></div>
+    <div className="loading-bar__box"></div>
+    <div className="loading-bar__box"></div>
+  </div>;
+};
+
+export default LoadingBar;
