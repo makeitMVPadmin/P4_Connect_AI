f10c8acb9ac0721d9580fa75f2b858f14bc9959d
MepilLeaf <77562979+MepilLeaf@users.noreply.github.com>
2024-07-24 13:45:28 -0700

    JH-create base for loading page

--- src/pages/LoadingPage/LoadingPage.jsx
+++ src/pages/LoadingPage/LoadingPage.jsx
@@ -1,8 +1,18 @@
 import "./LoadingPage.scss";
 import LoadingBar from "../../components/LoadingBar/LoadingBar";
+import DashboardNavbar from "../../components/DashboardNavbar/DashboardNavbar";
 
 const LoadingPage = () => {
-  return <LoadingBar />;
+  return (
+    <>
+      <DashboardNavbar />
+      <div className="loading-page">
+        <div className="loading-page__container">
+          <LoadingBar />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoadingPage;
