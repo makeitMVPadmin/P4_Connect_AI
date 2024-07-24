1a44e981f4002a0771c37b79d0e1c281d4960296
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-07-23 18:40:33 -0700

    AD-Quizpage progress

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -7,35 +7,53 @@ import DropdownCheckbox from "../../components/DropdownCheckbox/DropdownCheckbox
 import Textarea from "../../components/Textarea/Textarea";
 import React from "react";
 const QuizPage = () => {
-  const [formData, setFormData] = React.useState(() => {
-    const initialFormData = {};
-    QA.forEach((question) => {
-      initialFormData[question.question_content] =
-        question.question_type == "checkbox" ? [] : "";
-    });
-    return initialFormData;
-  });
-
+  // const [formData, setFormData] = React.useState(() => {
+  //   const initialFormData = {};
+  //   QA.forEach((item) => {
+  //     initialFormData[item.question_content] =
+  //       item.question_type == "checkbox" ? [] : "";
+  //   });
+  //   return initialFormData;
+  // });
+  const [formData, setFormData] = React.useState([]);
   const handleFormSubmit = (e) => {
     e.preventDefault();
     console.log("Form Data:", formData);
   };
 
-  const handleInputChange = (question, value) => {
-    setFormData({ ...formData, [question]: value });
+  const handleInputChange = (value) => {
+    let updatedValue = [];
+    console.log("value", value);
+    if (Array.isArray(value)) {
+      updatedValue = value.flatMap((item) => {
+        const answer_id = QA.map((data) =>
+          data.answers
+            ?.filter((answer) => answer.answer_content === item)
+            .map((answer) => answer.answer_id)
+        );
+        return answer_id;
+      });
+    } else {
+      updatedValue = QA.flatMap((item) =>
+        item.answers
+          ?.filter((answer) => answer.answer_content === value)
+          .map((answer) => answer.answer_id)
+      );
+    }
+    console.log("ppk", updatedValue);
+    setFormData([...formData, updatedValue]);
   };
 
   const renderedQuestions = (item, index) => {
     const answers = item.answers?.map((answer) => answer.answer_content);
+
     switch (item.question_type) {
       case "dropdown":
         return (
           <Dropdown
             labelName={item.question_content}
             dropDownInfo1={answers}
-            onChangeDropdown={(value) =>
-              handleInputChange(item.question_content, value)
-            }
+            onChangeDropdown={(value) => handleInputChange(value)}
           />
         );
       case "checkbox":
@@ -43,18 +61,14 @@ const QuizPage = () => {
           <DropdownCheckbox
             labelName={item.question_content}
             options1={answers}
-            onChangeDropdownCheckbox={(value) =>
-              handleInputChange(item.question_content, value)
-            }
+            onChangeDropdownCheckbox={(value) => handleInputChange(value)}
           />
         );
       case "textarea":
         return (
           <Textarea
             labelName={item.question_content}
-            handleTextarea={(value) =>
-              handleInputChange(item.question_content, value)
-            }
+            handleTextarea={(value) => handleInputChange(value)}
           />
         );
       default:
