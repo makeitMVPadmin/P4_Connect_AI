cef254e739fdebd5ae6041028e8cee4333cca1f2
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-07-24 21:15:38 -0700

    AD-buttonClicking at Promptpage done

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,12 @@
 import "./Button.scss";
 
 const Button = ({ text, color, eventListener, className }) => {
+  console.log(eventListener);
   return (
-    <button className={` button button--${color} ${className}`} onClick={eventListener}>
+    <button
+      className={` button button--${color} ${className}`}
+      onClick={eventListener}
+    >
       <h3 className="button__text">{text}</h3>
     </button>
   );
--- src/pages/PromptPage/PromptPage.jsx
+++ src/pages/PromptPage/PromptPage.jsx
@@ -2,7 +2,17 @@ import "./PromptPage.scss";
 import DashboardNavbar from "../../components/DashboardNavbar/DashboardNavbar";
 import Button from "../../components/Button/Button";
 import CoffeeMugWithHat_happy from "../../assets/images/coffeeMugWithHat_happy.svg";
+import { useState } from "react";
+import QuizPage from "../QuizPage/QuizPage";
 const PromptPage = () => {
+  const [currentPage, setCurrentPage] = useState("prompt");
+
+  const handleClick = () => {
+    setCurrentPage("quiz");
+  };
+  if (currentPage === "quiz") {
+     return <QuizPage />;
+  }
   return (
     <div>
       <div className="promptpage__navbar">
@@ -15,16 +25,34 @@ const PromptPage = () => {
           <div className="promptpage__middle-container">
             <div className="promptpage__middle-container__sub-container">
               <div className="promptpage__middle-container__coffeemug-container">
-                <img className="promptpage__middle-container__coffeemug-img" alt="" src={CoffeeMugWithHat_happy} />
+                <img
+                  className="promptpage__middle-container__coffeemug-img"
+                  alt=""
+                  src={CoffeeMugWithHat_happy}
+                />
               </div>
               <div className="promptpage__middle-container__sub-container text-container">
                 <div className="welcome-message">Welcome to ConnectAI</div>
-                <div className="welcome-text">Our AI feature empowers you to achieve your professional goals through personalized accountability partnerships. By taking our quick matching quiz, you’ll be paired with a peer who complements your skills, and you both will work towards a common goal.</div>
-                <div className="welcome-text">Click "Next" to get started on your journey to effective peer-to-peer accountability.</div>
+                <div className="welcome-text">
+                  Our AI feature empowers you to achieve your professional goals
+                  through personalized accountability partnerships. By taking
+                  our quick matching quiz, you’ll be paired with a peer who
+                  complements your skills, and you both will work towards a
+                  common goal.
+                </div>
+                <div className="welcome-text">
+                  Click "Next" to get started on your journey to effective
+                  peer-to-peer accountability.
+                </div>
               </div>
               <div className="button-container">
-                <Button text="Back" color="white" className="back"/>
-                <Button text="Next" color="dark-blue" className="next"/>
+                <Button text="Back" color="white" className="back" />
+                <Button
+                  text="Next"
+                  color="dark-blue"
+                  className="next"
+                  eventListener={handleClick}
+                />
               </div>
             </div>
           </div>
