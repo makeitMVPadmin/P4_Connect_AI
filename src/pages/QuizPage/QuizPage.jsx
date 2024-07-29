1d995e72eef409aceaea8d713a1c1fa99393b384
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-07-29 11:39:16 -0700

    Ad-QuizQuestions

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -6,94 +6,95 @@ import Dropdown from "../../components/Dropdown/Dropdown";
 import DropdownCheckbox from "../../components/DropdownCheckbox/DropdownCheckbox";
 import Textarea from "../../components/Textarea/Textarea";
 import React from "react";
+import QuizQuestions from "../../components/QuizQuestions/QuizQuestions";
 const QuizPage = () => {
-  // const [formData, setFormData] = React.useState(() => {
-  //   const initialFormData = {};
-  //   QA.forEach((item) => {
-  //     initialFormData[item.question_content] =
-  //       item.question_type == "checkbox" ? [] : "";
-  //   });
-  //   return initialFormData;
-  // });
-  const [formData, setFormData] = React.useState([]);
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form Data:", formData);
-  };
+  // // const [formData, setFormData] = React.useState(() => {
+  // //   const initialFormData = {};
+  // //   QA.forEach((item) => {
+  // //     initialFormData[item.question_content] =
+  // //       item.question_type == "checkbox" ? [] : "";
+  // //   });
+  // //   return initialFormData;
+  // // });
+  // const [formData, setFormData] = React.useState([]);
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log("Form Data:", formData);
+  // };
 
-  const handleInputChange = (question_type, question_content, value) => {
-    let updatedValue = new Set(formData);
-    const QuestionItem = QA.filter(
-      (data) => data.question_content === question_content
-    );
-    const allAns = QuestionItem.flatMap((item) => item.answers);
+  // const handleInputChange = (question_type, question_content, value) => {
+  //   let updatedValue = new Set(formData);
+  //   const QuestionItem = QA.filter(
+  //     (data) => data.question_content === question_content
+  //   );
+  //   const allAns = QuestionItem.flatMap((item) => item.answers);
 
-    if (question_type === "checkbox") {
-      value.flatMap((eachAns) => {
-        const ansIds = allAns
-          .filter((answer) => answer.answer_content === eachAns)
-          .map((answer) => answer.answer_id);
-        ansIds.forEach((id) => updatedValue.add(id));
-      });
-    } else if (question_type === "dropdown") {
-      const ansIds = allAns
-        .filter((answer) => answer.answer_content === value)
-        .map((answer) => answer.answer_id);
-      ansIds.forEach((id) => updatedValue.add(id));
-    } else {
-      updatedValue.add(value);
-    }
-    setFormData(updatedValue);
-  };
+  //   if (question_type === "checkbox") {
+  //     value.flatMap((eachAns) => {
+  //       const ansIds = allAns
+  //         .filter((answer) => answer.answer_content === eachAns)
+  //         .map((answer) => answer.answer_id);
+  //       ansIds.forEach((id) => updatedValue.add(id));
+  //     });
+  //   } else if (question_type === "dropdown") {
+  //     const ansIds = allAns
+  //       .filter((answer) => answer.answer_content === value)
+  //       .map((answer) => answer.answer_id);
+  //     ansIds.forEach((id) => updatedValue.add(id));
+  //   } else {
+  //     updatedValue.add(value);
+  //   }
+  //   setFormData(updatedValue);
+  // };
 
-  const renderedQuestions = (item, index) => {
-    const answers = item.answers?.map((answer) => answer.answer_content);
+  // const renderedQuestions = (item, index) => {
+  //   const answers = item.answers?.map((answer) => answer.answer_content);
 
-    switch (item.question_type) {
-      case "dropdown":
-        return (
-          <Dropdown
-            labelName={item.question_content}
-            dropDownInfo1={answers}
-            onChangeDropdown={(value) =>
-              handleInputChange(
-                item.question_type,
-                item.question_content,
-                value
-              )
-            }
-          />
-        );
-      case "checkbox":
-        return (
-          <DropdownCheckbox
-            labelName={item.question_content}
-            options1={answers}
-            onChangeDropdownCheckbox={(value) =>
-              handleInputChange(
-                item.question_type,
-                item.question_content,
-                value
-              )
-            }
-          />
-        );
-      case "textarea":
-        return (
-          <Textarea
-            labelName={item.question_content}
-            handleTextarea={(value) =>
-              handleInputChange(
-                item.question_type,
-                item.question_content,
-                value
-              )
-            }
-          />
-        );
-      default:
-    }
-  };
+  //   switch (item.question_type) {
+  //     case "dropdown":
+  //       return (
+  //         <Dropdown
+  //           labelName={item.question_content}
+  //           dropDownInfo1={answers}
+  //           onChangeDropdown={(value) =>
+  //             handleInputChange(
+  //               item.question_type,
+  //               item.question_content,
+  //               value
+  //             )
+  //           }
+  //         />
+  //       );
+  //     case "checkbox":
+  //       return (
+  //         <DropdownCheckbox
+  //           labelName={item.question_content}
+  //           options1={answers}
+  //           onChangeDropdownCheckbox={(value) =>
+  //             handleInputChange(
+  //               item.question_type,
+  //               item.question_content,
+  //               value
+  //             )
+  //           }
+  //         />
+  //       );
+  //     case "textarea":
+  //       return (
+  //         <Textarea
+  //           labelName={item.question_content}
+  //           handleTextarea={(value) =>
+  //             handleInputChange(
+  //               item.question_type,
+  //               item.question_content,
+  //               value
+  //             )
+  //           }
+  //         />
+  //       );
+  //     default:
+  //   }
+  // };
 
   return (
     <div className="quizpage">
@@ -102,7 +103,7 @@ const QuizPage = () => {
           <ProgressBar />
         </div>
         <div className="quizpage__main">
-          <form onSubmit={handleFormSubmit}>
+          {/* <form onSubmit={handleFormSubmit}>
             <section className="quizpage__main_section">
               {QA.map((item, index) => (
                 <div className="quizpage__main_section--field">
@@ -113,7 +114,9 @@ const QuizPage = () => {
                 <Button text="Next" color="blue" />
               </div>
             </section>
-          </form>
+          
+          </form> */}
+          <QuizQuestions/>
         </div>
       </div>
     </div>
