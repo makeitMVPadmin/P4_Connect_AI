cc7780f0f30d1a06e859e1d5ac39d46c530e40bb
Avi Brar <avineetsbrar@gmail.com>
2024-07-31 13:30:32 -0700

    firebase-finally initiated and basic functions and layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./main.js";
 import PromptPage from "./pages/PromptPage/PromptPage";
+import BackEndTest from "./pages/BackEnd/Backend.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/prompt" element={<PromptPage />} />
+        <Route path="/test" element={<BackEndTest />} />
       </Routes>
     </>
   );
