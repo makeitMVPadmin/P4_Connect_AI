cd298ef8b49d0317477b45d12d16ae4d92864bcd
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-07-31 15:55:09 -0700

    Ad

--- src/components/QuizQuestions/QuizQuestions.jsx
+++ src/components/QuizQuestions/QuizQuestions.jsx
@@ -7,14 +7,7 @@ import DropdownCheckbox from "../../components/DropdownCheckbox/DropdownCheckbox
 import Textarea from "../../components/Textarea/Textarea";
 import "./QuizQuestions.scss";
 const QuizQuestions = ({ setCurrentPage, onProgressChange }) => {
-  const [formData, setFormData] = React.useState(() => {
-    const initialFormData = {};
-    QA.forEach((item) => {
-      initialFormData[item.question_content] =
-        item.question_type == "checkbox" ? [] : "";
-    });
-    return initialFormData;
-  });
+  const [formData, setFormData] = React.useState([]);
   const requiredQuestionIds = ["001", "002", "004", "006", "008"];
   const [selectedAnswerIds, setSelectedAnswerIds] = useState([]);
   const [answeredQuestions, setAnsweredQuestions] = React.useState(new Set());
@@ -27,44 +20,79 @@ const QuizQuestions = ({ setCurrentPage, onProgressChange }) => {
   };
 
   const handleInputChange = (question_type, question_content, value) => {
-    setFormData({ ...formData, [question_content]: value });
+    //setFormData({ ...formData, [question_content]: value });
 
-    const questionItem = QA.find(
-      (data) => data.question_content === question_content
-    );
+    const questionItem = QA.find((data) => data.question_content === question_content);
     const allAns = questionItem.answers;
 
-    let newSelectedAnswerIds = selectedAnswerIds;
+    let newSelectedAnswerIds = [...selectedAnswerIds];
+    let newFormData = [...formData];
+
 
     if (question_type == "checkbox") {
       const answerIds = value.map((each) => {
         const found = allAns.filter((data) => data.answer_content == each);
-        return found[0]?.answer_id;
+        return {
+          question_content,
+          answer_id: found[0].answer_id,
+          answer_content: found[0].answer_content,
+        };
       });
       console.log("answerIds for checkbox", answerIds);
       console.log("previously selected ids for checkbox", newSelectedAnswerIds);
+
       newSelectedAnswerIds = newSelectedAnswerIds.filter(
         (id) => !allAns.some((ans) => ans.answer_id === id)
       );
-console.log("After discarding all ids from checkbox for a particular question",newSelectedAnswerIds);
-      newSelectedAnswerIds = [newSelectedAnswerIds, answerIds.flat()];
+      console.log(
+        "After discarding all ids from checkbox for a particular question",
+        newSelectedAnswerIds
+      );
+      newSelectedAnswerIds = [...newSelectedAnswerIds, answerIds.answer_id];
+      newFormData = newFormData.filter(
+        (item) => item.question_content !== question_content
+      );
+      newFormData = [...newFormData, answerIds.flat()];
+    } 
     
-    } else if (question_type == "dropdown") {
+    else if (question_type == "dropdown")
+      
+      {
       const answerIds = allAns
-        .filter((item) => item.answer_content === value)
-        .map((data) => data.answer_id);
-     // console.log("answerIds in dropdown", answerIds);
+        .filter((item) => {
+          return item.answer_content === value;
+        })
+        .map((ans) => {
+          return {
+            question_content,
+            answer_id: ans.answer_id,
+            answer_content: ans.answer_content,
+          };
+        });
+
+      console.log("answerIds in dropdown", answerIds);
 
       newSelectedAnswerIds = newSelectedAnswerIds.filter(
         (id) => !allAns.some((ans) => ans.answer_id === id)
       );
 
-      newSelectedAnswerIds = [...newSelectedAnswerIds, answerIds];
-    } else {
+      newSelectedAnswerIds = [...newSelectedAnswerIds, answerIds[0].answer_id];
+      console.log("newSelectedAnswerIds in dropdown", newSelectedAnswerIds);
+
+      newFormData = newFormData.filter(
+        (data) => data.question_content !== question_content
+      );
+      newFormData = [...newFormData, answerIds[0]];
+      console.log("newFormData in dropdown", newFormData);
+    } 
+    
+    else {
       newSelectedAnswerIds = [...newSelectedAnswerIds, value];
     }
 
     setSelectedAnswerIds(newSelectedAnswerIds.flat());
+    setFormData(newFormData);
+
 
     setAnsweredQuestions((prev) => {
       const newSet = new Set(prev);
@@ -73,17 +101,17 @@ console.log("After discarding all ids from checkbox for a particular question",n
     });
   };
 
-  const arequestionAnswered = () => {
-    const k = Array.from(answeredQuestions).map((ans) => {
-      const p = QA.find((data) => data.question_content === ans).question_id;
-      return p;
-    });
+  // const arequestionAnswered = () => {
+  //   const k = Array.from(answeredQuestions).map((ans) => {
+  //     const p = QA.find((data) => data.question_content === ans).question_id;
+  //     return p;
+  //   });
 
-    const answeredQuestArray = requiredQuestionIds.map((id) => k.includes(id));
-    const answeredQuest = answeredQuestArray.every((quest) => quest === true);
-    //console.log("answeredQuest", answeredQuest);
-    return answeredQuest;
-  };
+  //   const answeredQuestArray = requiredQuestionIds.map((id) => k.includes(id));
+  //   const answeredQuest = answeredQuestArray.every((quest) => quest === true);
+  //   //console.log("answeredQuest", answeredQuest);
+  //   return answeredQuest;
+  // };
 
   useEffect(() => {
     onProgressChange(answeredQuestions.size);
@@ -153,9 +181,9 @@ console.log("After discarding all ids from checkbox for a particular question",n
           <div className="quizquestions_section--field">
             <Button
               text="Next"
-              color={arequestionAnswered() ? "blue" : "grey"}
+              color="blue"
               type="submit"
-              disabled={!arequestionAnswered()}
+              // disabled={!arequestionAnswered()}
             />
           </div>
         </section>
