0a8b4aa38e761e9a68bd4df39671bee556c372ce
Gloria Lau <glorialau@live.ca>
2024-08-07 14:34:57 -0700

    GL-fix project name spelling

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import "./styles/partials/_global.scss";
 const Home = () => {
   return (
     <div className="home">
-      <h1 className="home__title"> Welcome to Acooutabuddy!</h1>
-      <Link to="/prompt">Click to go to Acooutabuddy</Link>
+      <h1 className="home__title"> Welcome to Accountabuddy!</h1>
+      <Link to="/prompt">Click to go to Accountabuddy</Link>
     </div>
   );
 };
