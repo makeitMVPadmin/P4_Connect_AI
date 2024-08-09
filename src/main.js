e78e9a2632642d5da97b3dffb7aa24fa75e26d47
Gloria Lau <glorialau@live.ca>
2024-08-09 09:23:36 -0700

    GL-fix Accountabuddy spelling, change to AccountaBuddy

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import "./styles/partials/_global.scss";
 const Home = () => {
   return (
     <div className="home">
-      <h1 className="home__title"> Welcome to Accountabuddy!</h1>
-      <Link to="/prompt">Click to go to Accountabuddy</Link>
+      <h1 className="home__title"> Welcome to AccountaBuddy!</h1>
+      <Link to="/prompt">Click to go to AccountaBuddy</Link>
     </div>
   );
 };
