3cd8fb63b44dab11937ff1a9e050ad845bab012c
jagadeepavula <jagadeepavula@gmail.com>
2024-08-11 10:48:13 -0700

    JA- changed "roadmap" to "prompt" in promptpage

--- src/pages/PromptPage/PromptPage.jsx
+++ src/pages/PromptPage/PromptPage.jsx
@@ -10,13 +10,13 @@ import LoadingPage from "../../components/LoadingPage/LoadingPage";
 
 const PromptPage = () => {
   const navigate = useNavigate();
-  const [currentPage, setCurrentPage] = useState("roadmap");
+  const [currentPage, setCurrentPage] = useState("prompt");
 
   // ACCESS MATCH ALGO RESULTS through a state passed into quizPage
   const [matchResults, setMatchResults] = useState(null);
 
   useEffect(() => {
-    setCurrentPage("roadmap");
+    setCurrentPage("prompt");
     sessionStorage.removeItem("formData");
     sessionStorage.removeItem("answeredQuestions");
     sessionStorage.removeItem("selectedAnswerIdsJSON");
