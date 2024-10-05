31eb0fbcfc8b58149d2abdeac5cf99e3dedc9f47
Ali <anix1@kent.edu>
2024-10-05 16:49:40 -0600

    refactor magic string and use constants.js

--- src/backend/Backend.jsx
+++ src/backend/Backend.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { COLLECTIONS } from "../constants.js";
 import {
   createData,
   readData,
@@ -9,7 +10,7 @@ import {
 const BackEndTest = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
-  const collectionName = "Users";
+  const collectionName = COLLECTIONS.USERS;
 
   const fetchDataAndUpdateState = async () => {
     const result = await readData(collectionName);
