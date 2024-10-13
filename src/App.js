b21cb22e5c45e6922f63f9078721ed1dbca6b2d0
Ali <anix1@kent.edu>
2024-10-13 16:57:09 -0600

    wrap PromptPage with PageProvider for context and for components to consume data

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import PromptPage from "./pages/PromptPage/PromptPage";
 import BackEndTest from "./backend/Backend.jsx";
 import AiBackendTest from "./backend/AiBackendTest.js";
 import LoadingPage from "./pages/LoadingPage/LoadingPage.jsx";
-
+import { PageProvider } from "./contexts/PageContext";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/prompt" element={<PromptPage />} />
+        <Route
+          path="/prompt"
+          element={
+            <PageProvider>
+              <PromptPage />
+            </PageProvider>
+          }
+        />
         <Route path="/test" element={<BackEndTest />} />
         <Route path="/aitest" element={<AiBackendTest />} />
         <Route path="/Testa" element={<LoadingPage />} />
