d14c9ee3d0aa74ba37d456a7a72b7a2ef1601800
jagadeepavula <jagadeepavula@gmail.com>
2024-10-17 18:26:59 -0700

    inegration challenge page with filterboard

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,27 +1,11 @@
 import "./TestPage.scss";
 import Button from "@mui/material/Button";
-// import Modal from '@mui/material/Modal';
 import { useState } from "react";
 import Modal from "react-modal";
-import { PopUpModal, PopUpStyle } from "../../components/PopUpModal/PopUpModal";
+import { PopUpModal } from "../../components/PopUpModal/PopUpModal";
 import LoadingPage from "../LoadingPage/LoadingPage";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
 const TestPage = () => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
   const [isModalOpen, setModalOpen] = useState(false);
 
   const handleOpenPostModal = () => {
