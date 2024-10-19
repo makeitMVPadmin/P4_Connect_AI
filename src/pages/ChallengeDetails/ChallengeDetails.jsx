834a13f989eaf11deea47bd0e6f30edab8201251
Tim Wallace <timwallacedev@gmail.com>
2024-10-19 17:47:28 -0400

    integrate challenge details page with firebase

--- src/pages/ChallengeDetails/ChallengeDetails.jsx
+++ src/pages/ChallengeDetails/ChallengeDetails.jsx
@@ -4,148 +4,175 @@ import DashboardNavbar from "../../components/DashboardNavbar/DashboardNavbar";
 import clock from "../../assets/icons/clock.svg";
 import pen from "../../assets/icons/pen.svg";
 import bulb from "../../assets/icons/lightbulb.svg";
-import { MOCK_CHALLENGES } from "../../mock-data/mockChallenges";
+import { db } from "../../firebase";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
 
 export function ChallengeDetails() {
-  const { challengeId } = useParams();
 
-  const challengeData = MOCK_CHALLENGES.challenges.find(
-    (challenge) => challenge.id === parseInt(challengeId)
-  );
+    const { challengeId } = useParams();
 
-  return (
-    <>
-      <DashboardNavbar />
-      <main className="main">
-        <img
-          src="https://png.pngtree.com/background/20230517/original/pngtree-what-can-a-home-computer-desk-look-like-picture-image_2625455.jpg"
-          className="image"
-          alt="computer on a desk"
-        />
+    const [challengeData, setChallengeData] = useState(null);
 
-        <section className="challenge">
-          <div className="overview">
-            <div className="overview__difficulty">
-              {challengeData.difficulty}
-            </div>
-            <h1 className="overview__title">{challengeData.task}</h1>
-            <p className="overview__overview">
-              {challengeData.detailedProblem.description}
-            </p>
+    const navigate = useNavigate();
 
-            <div className="overview__details">
-              <div className="overview__type">
-                <img src={pen} className="overview__icon" alt="pen icon" />
-                <span className="overview__type-text">
-                  {challengeData.category}
-                </span>
-              </div>
-              <div className="overview__type">
-                <img src={clock} className="overview__icon" alt="pen icon" />
-                <span className="overview__type-text">
-                  {challengeData.duration}
-                </span>
-              </div>
-              <div className="overview__type">
-                <img src={bulb} className="overview__icon" alt="pen icon" />
-                <span className="overview__type-text">VSCode / Figma</span>
-              </div>
-            </div>
+    useEffect(() => {
+        async function getChallengeData() {
 
-            <div className="objectives">
-              <h2 className="objectives__title">Constraints</h2>
-              <ul className="objectives__list">
-                {challengeData.detailedProblem.constraints.map(
-                  (constraint, index) => {
-                    return (
-                      <li className="objectives__item" key={index}>
-                        {constraint}
-                      </li>
-                    );
-                  }
-                )}
-              </ul>
-            </div>
+            const docRef = doc(db, "Challenges", challengeId);
+            const docSnap = await getDoc(docRef);
 
-            <div className="examples">
-              <h2 className="examples__title">Examples</h2>
-              <ul className="examples__list">
-                {challengeData.detailedProblem.examples.map(
-                  (example, index) => {
-                    return (
-                      <code className="examples__item" key={index}>
-                        Input: {example.input} <br /> Output: {example.output}
-                      </code>
-                    );
-                  }
-                )}
-              </ul>
-            </div>
+            if (docSnap.exists()) {
+                const data = docSnap.data()
+                console.log(data)
+                setChallengeData(data);
+            } else {
+                navigate("/testPage")
+            }
+        }
 
-            <div className="solutions">
-              <h2 className="solutions__title">Solution Approach</h2>
-              <ul className="solutions__list">
-                {challengeData.detailedProblem.solutionApproach.map(
-                  (example, index) => {
-                    return (
-                      <li className="solutions__item" key={index}>
-                        {example}
-                      </li>
-                    );
-                  }
-                )}
-              </ul>
-            </div>
+        getChallengeData()
+    }, [challengeId, navigate])
 
-            <div className="action">
-              <Link to="/pair">
-                <button className="action__button">Pair Up</button>
-              </Link>
-              <Link to="/testPage">
-                <button className="action__button action__button--secondary">
-                  Dashboard
-                </button>
-              </Link>
-            </div>
-          </div>
 
-          <div className="instructions">
-            <div className="instructions__step">
-              <div className="instructions__chip  instructions__chip--green">
-                Complete the challenge
-              </div>
-              <ul className="instructions__list">
-                <li className="instructions__item">
-                  Create a solution that works with all the examples. Make sure
-                  your solution adheres the all constraints given.
-                </li>
-              </ul>
-            </div>
-            <div className="instructions__step">
-              <div className="instructions__chip  instructions__chip--yellow">
-                Submit Your Code
-              </div>
-              <ul className="instructions__list">
-                <li className="instructions__item">
-                  After completing the challenge, input your code into the
-                  designated User Code section and click Submit.
-                </li>
-              </ul>
-            </div>
-            <div className="instructions__step">
-              <div className="instructions__chip instructions__chip--blue">
-                Finalize your solution
-              </div>
-              <ul className="instructions__list">
-                <li className="instructions__item">
-                  After receiving feedback, implement any improvements that have
-                  been suggested by your partner.
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
-      </main>
-    </>
-  );
+    if (!challengeData) {
+        return <div>Loading</div>;
+    }
+
+    return (
+        <>
+            <DashboardNavbar />
+            <main className="main">
+                <img
+                    src="https://png.pngtree.com/background/20230517/original/pngtree-what-can-a-home-computer-desk-look-like-picture-image_2625455.jpg"
+                    className="image"
+                    alt="computer on a desk"
+                />
+
+                <section className="challenge">
+                    <div className="overview">
+                        <div className="overview__difficulty">
+                            {challengeData.difficulty}
+                        </div>
+                        <h1 className="overview__title">{challengeData.task}</h1>
+                        <p className="overview__overview">
+                            {challengeData.detailedProblem.description}
+                        </p>
+
+                        <div className="overview__details">
+                            <div className="overview__type">
+                                <img src={pen} className="overview__icon" alt="pen icon" />
+                                <span className="overview__type-text">
+                                    {challengeData.category}
+                                </span>
+                            </div>
+                            <div className="overview__type">
+                                <img src={clock} className="overview__icon" alt="pen icon" />
+                                <span className="overview__type-text">
+                                    {challengeData.duration}
+                                </span>
+                            </div>
+                            <div className="overview__type">
+                                <img src={bulb} className="overview__icon" alt="pen icon" />
+                                <span className="overview__type-text">VS Code, {challengeData.category}</span>
+                            </div>
+                        </div>
+
+                        <div className="objectives">
+                            <h2 className="objectives__title">Constraints</h2>
+                            <ul className="objectives__list">
+                                {challengeData.detailedProblem.constraints.map(
+                                    (constraint, index) => {
+                                        return (
+                                            <li className="objectives__item" key={index}>
+                                                {constraint}
+                                            </li>
+                                        );
+                                    }
+                                )}
+                            </ul>
+                        </div>
+
+                        <div className="examples">
+                            <h2 className="examples__title">Examples</h2>
+                            <ul className="examples__list">
+                                {challengeData.detailedProblem.examples.map(
+                                    (example, index) => {
+                                        return (
+                                            <code className="examples__item" key={index}>
+                                                Input: {example.input} <br /> Output: {example.output}
+                                            </code>
+                                        );
+                                    }
+                                )}
+                            </ul>
+                        </div>
+
+                        <div className="solutions">
+                            <h2 className="solutions__title">Solution Approach</h2>
+                            <ul className="solutions__list">
+                                {challengeData.detailedProblem.solutionApproach.map(
+                                    (example, index) => {
+                                        return (
+                                            <li className="solutions__item" key={index}>
+                                                {example}
+                                            </li>
+                                        );
+                                    }
+                                )}
+                            </ul>
+                        </div>
+
+                        <div className="action">
+                            <Link to="/testPage">
+                                <button className="action__button action__button--secondary">
+                                    Dashboard
+                                </button>
+                            </Link>
+                            <Link to="/pair">
+                                <button className="action__button">Pair Up</button>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className="instructions">
+                        <div className="instructions__step">
+                            <div className="instructions__chip  instructions__chip--green">
+                                Complete the challenge
+                            </div>
+                            <ul className="instructions__list">
+                                <li className="instructions__item">
+                                    Create a solution that works with all the examples. Make sure
+                                    your solution adheres the all constraints given.
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="instructions__step">
+                            <div className="instructions__chip  instructions__chip--yellow">
+                                Submit Your Code
+                            </div>
+                            <ul className="instructions__list">
+                                <li className="instructions__item">
+                                    After completing the challenge, input your code into the
+                                    designated User Code section and click Submit.
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="instructions__step">
+                            <div className="instructions__chip instructions__chip--blue">
+                                Finalize your solution
+                            </div>
+                            <ul className="instructions__list">
+                                <li className="instructions__item">
+                                    After receiving feedback, implement any improvements that have
+                                    been suggested by your partner.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </>
+    );
 }
