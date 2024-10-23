5634a93b5155737ea6d5bab900f07bf29d9bc4a8
Ali <anix1@kent.edu>
2024-10-23 13:11:59 -0600

    add challenge card banner images to assets folder and randomly display the images

--- src/components/ChallengeCard/ChallengeCard.jsx
+++ src/components/ChallengeCard/ChallengeCard.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./ChallengeCard.scss";
-import ChallengeCardBanner from "../../assets/images/ChallengeCardBanner.png";
+import ChallengeCardBanner1 from "../../assets/images/ChallengeCardBanner1.png";
+import ChallengeCardBanner2 from "../../assets/images/ChallengeCardBanner2.png";
+import ChallengeCardBanner3 from "../../assets/images/ChallengeCardBanner3.png";
+import ChallengeCardBanner4 from "../../assets/images/ChallengeCardBanner4.png";
+import ChallengeCardBanner5 from "../../assets/images/ChallengeCardBanner5.png";
+import ChallengeCardBanner6 from "../../assets/images/ChallengeCardBanner6.png";
+import ChallengeCardBanner7 from "../../assets/images/ChallengeCardBanner7.png";
+import ChallengeCardBanner8 from "../../assets/images/ChallengeCardBanner8.png";
+import ChallengeCardBanner9 from "../../assets/images/ChallengeCardBanner9.png";
 import Pen from "../../assets/images/Pen.svg";
 import Clock from "../../assets/images/Clock.png";
 import Idea from "../../assets/images/Idea.png";
@@ -14,16 +22,49 @@ const ChallengeCard = ({ challenge }) => {
       case "intermediate":
         return "#FFD22F";
       case "hard":
-        return "#FF5733";
+        return "#FF7070";
       default:
         return "#37AFFF";
+    }
+  };
+
+  const getRandomBannerImage = (difficulty) => {
+    const easyBanners = [
+      ChallengeCardBanner1,
+      ChallengeCardBanner2,
+      ChallengeCardBanner3,
+    ];
+    const intermediateBanners = [
+      ChallengeCardBanner4,
+      ChallengeCardBanner5,
+      ChallengeCardBanner6,
+    ];
+    const hardBanners = [
+      ChallengeCardBanner7,
+      ChallengeCardBanner8,
+      ChallengeCardBanner9,
+    ];
+    switch (difficulty.toLowerCase()) {
+      case "easy":
+        return easyBanners[Math.floor(Math.random() * easyBanners.length)];
+      case "intermediate":
+        return intermediateBanners[
+          Math.floor(Math.random() * intermediateBanners.length)
+        ];
+      case "hard":
+        return hardBanners[Math.floor(Math.random() * hardBanners.length)];
+      default:
+        return easyBanners[Math.floor(Math.random() * easyBanners.length)];
     }
   };
 
   return (
     <div className="challenge-card">
       <div className="image-container">
-        <img src={ChallengeCardBanner} alt="Challenge Card Banner" />
+        <img
+          src={getRandomBannerImage(challenge.difficulty)}
+          alt="Challenge Card Banner"
+        />
         <div
           className="difficulty-button"
           style={{ backgroundColor: getDifficultyColor(challenge.difficulty) }}
