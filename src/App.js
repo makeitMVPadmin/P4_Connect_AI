850c08dce68089b5095b640f615e63f9c079561c
Tim Wallace <timwallacedev@gmail.com>
2024-10-24 15:58:01 -0400

    create challenge details button with modal

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AiBackendTest from "./backend/AiBackendTest.js";
 import { ChallengeDetails } from "./pages/ChallengeDetails/ChallengeDetails.jsx";
 import LoadingPage from "./pages/LoadingPage/LoadingPage.jsx";
 import ChallengePage from "./components/P5AP_ChallengePage/ChallengePage.jsx";
+import TaskPageDetailedInstructions from "./components/TaskPageDetailedInstructions/TaskPageDetailedInstructions.jsx";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/aitest" element={<AiBackendTest />} />
         <Route path="/challenge/:challengeId" element={<ChallengeDetails />} />
         <Route path="/Testa" element={<LoadingPage />} />
+        <Route path="/details" element={<TaskPageDetailedInstructions id="9chM02f9llGdd6ZPj1CO"/>}/>
       </Routes>
     </PageProvider>
   );
