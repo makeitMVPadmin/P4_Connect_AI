ace779b0b7cffb6d8681125a93246e18fadaea4e
laxmik1112 <kuntojil@oregonstate.edu>
2024-10-25 16:46:43 -0700

    Resolved the review comments and fixed the stepper button color

--- src/components/P5ChallengeTaskStepper/P5ChallengeTaskStepper.jsx
+++ src/components/P5ChallengeTaskStepper/P5ChallengeTaskStepper.jsx
@@ -72,43 +72,13 @@ const P5ChallengeTaskStepper = () => {
     }
   };
   return (
-    // <div className="task-stepper-container">
-    //   {/* Connect with Partner Button */}
-    //   <div className="step-button">
-    //     <div className="button-text">Connect with Partner</div>
-    //   </div>
-
-    //   {/* Task Details */}
-    //   <div className="task-details">
-    //     {/* LinkedIn Section */}
-    //     <div className="task-section">
-    //       <div className="section-title">LinkedIn</div>
-    //       <div className="section-description">
-    //         Connect with your partner by using the LinkedIn button located in
-    //         the right tab next to “Connect.”
-    //         <br />
-    //         Alternatively, you can look them up by their username.
-    //       </div>
-    //     </div>
-
-    //     {/* Google Meet Section */}
-    //     <div className="task-section">
-    //       <div className="section-title">Google Meet</div>
-    //       <div className="section-description">
-    //         Use the Google Meet button in the right tab next to “Connect.” This
-    //         will direct you to Google Meet, where a meeting template will be
-    //         prefilled, making it easier to schedule a session with your partner.
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="task-stepper-container">
       {taskDetails.steps.map((step, index) => (
         <>
           <div key={index} className="step-section">
             {/* Dynamic Button for each step */}
             <div
-              className={`step-button ${currentStep == index ? 'completed' : ''}`}
+              className={`step-button ${currentStep > index ? "completed" : currentStep === index ? "active" : ""}`}
               style={{ backgroundColor: step.buttonColor }}
             >
               <div className="button-text">{step.stepTitle}</div>
@@ -135,9 +105,18 @@ const P5ChallengeTaskStepper = () => {
         <button className="dashboard-button" onClick={handleDashboard}>
           <span className="dashboard-text">Dashboard</span>
         </button>
-        <button className={`next-button ${currentStep >= 2 ? 'completed' : ''}`} onClick={handleNextStep}>
+        {/* <button className={`next-button ${currentStep >= 2 ? 'completed' : ''}`} onClick={handleNextStep}>
           <span className="next-text">Next Step</span>
-        </button>
+        </button> */}
+        {currentStep >= 3 ? (
+          <button className={`next-button completed`}>
+            <span className="next-text">Completed</span>
+          </button>
+        ) : (
+          <button className={`next-button`} onClick={handleNextStep}>
+            <span className="next-text">Next Step</span>
+          </button>
+        )}
       </div>
     </div>
   );
